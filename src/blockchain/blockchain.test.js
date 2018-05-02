b45dc1b72f8c15c7c9b292acdd89694c2ac30232
Pedro Reis <pedroreisv@gmail.com>
2018-05-02 12:32:40 -0300

    Add blockchain class + unit tests

---
+++ src/blockchain/blockchain.test.js
@@ -0,0 +1,82 @@
+const Blockchain = require("../blockchain");
+const Block = require("../block");
+
+describe("Classe Blockchain", () => {
+  let blockchain1, blockchain2;
+  const blockData1 = "bloco 1";
+  const blockData2 = "bloco 2";
+
+  beforeEach(() => {
+    blockchain1 = new Blockchain();
+    blockchain2 = new Blockchain();
+  });
+
+  describe("construtor", () => {
+    it("blockchain deve ter a estrutura de dados correta", () => {
+      expect(testBlockchainDataStructure(blockchain1)).toBe(true);
+    });
+
+    it("blockchain deve começar com o bloco genesis", () => {
+      expect(blockchain1.chain[0]).toEqual(Block.genesis());
+    });
+  });
+
+  describe("método addBlock", () => {
+    it("deve adicionar novo bloco à blockchain", () => {
+      const data = "dados do novo bloco";
+      blockchain1.addBlock(data);
+      const lastBlock = blockchain1.chain[blockchain1.chain.length - 1];
+
+      expect(blockchain1.chain.length).toBe(2);
+      expect(lastBlock.data).toBe(data);
+    });
+  });
+
+  describe("método isChainValid", () => {
+    it("deve invalidar blockchain com bloco genesis modificado", () => {
+      blockchain1.chain[0].data = "Não-Genesis";
+
+      expect(blockchain2.isChainValid(blockchain1.chain)).toBe(false);
+    });
+
+    it("deve invalidar blockchain com bloco diferente do genesis modificado", () => {
+      blockchain1.addBlock(blockData1);
+      blockchain1.chain[1].data = blockData2;
+
+      expect(blockchain2.isChainValid(blockchain1.chain)).toBe(false);
+    });
+
+    it("deve validar blockchain válida", () => {
+      blockchain1.addBlock(blockData1);
+
+      expect(blockchain2.isChainValid(blockchain1.chain)).toBe(true);
+    });
+  });
+
+  describe("método replaceChain", () => {
+    it("deve substituir blockchain por uma maior e válida", () => {
+      blockchain1.addBlock(blockData1);
+      blockchain2.replaceChain(blockchain1.chain);
+
+      expect(blockchain2.chain).toBe(blockchain1.chain);
+    });
+
+    it("não deve substituir blockchain por uma menor", () => {
+      blockchain1.addBlock(blockData1);
+      blockchain1.replaceChain(blockchain2.chain);
+
+      expect(blockchain1.chain).not.toBe(blockchain2.chain);
+    });
+
+    it("não deve substituir blockchain por uma inválida", () => {
+      blockchain1.addBlock(blockData1);
+      blockchain1.chain[1].data = blockData2;
+      blockchain2.replaceChain(blockchain1.chain);
+
+      expect(blockchain2.chain).not.toBe(blockchain1.chain);
+    });
+  });
+});
+
+const testBlockchainDataStructure = blockchain =>
+  Array.isArray(blockchain.chain);
---
+++ src/blockchain/index.js
@@ -0,0 +1,52 @@
+const Block = require("../block");
+
+class Blockchain {
+  constructor() {
+    this.chain = [Block.genesis()];
+  }
+
+  addBlock(data) {
+    const lastBlock = this.chain[this.chain.length - 1];
+    const block = Block.mine(lastBlock, data);
+    this.chain.push(block);
+
+    console.log(`
+    Bloco adicionado:
+    ${block.toString()}
+    `);
+
+    return block;
+  }
+
+  isChainValid(chain) {
+    return chain.every((block, index) => {
+      if (index === 0) {
+        return JSON.stringify(chain[0]) === JSON.stringify(Block.genesis());
+      }
+
+      const lastBlock = chain[index - 1];
+
+      return (
+        block.lastHash === lastBlock.hash &&
+        block.hash === Block.getBlockHash(block)
+      );
+    });
+  }
+
+  replaceChain(newChain) {
+    if (newChain.length <= this.chain.length) {
+      console.log("A nova blockchain não é maior que a blockchain atual.");
+      return;
+    }
+
+    if (!this.isChainValid(newChain)) {
+      console.log("A nova blockchain não é válida.");
+      return;
+    }
+
+    console.log("Substituindo a block chain atual pela nova blockchain.");
+    this.chain = newChain;
+  }
+}
+
+module.exports = Blockchain;
