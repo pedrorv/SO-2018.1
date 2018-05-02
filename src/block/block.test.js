64ec7732b43ea54fb065c14e3f930bd1b3e62fc1
Pedro Reis <pedroreisv@gmail.com>
2018-05-02 10:57:25 -0300

    Add block class + unit tests

---
+++ src/block/block.test.js
@@ -0,0 +1,51 @@
+const Block = require("../block");
+
+describe("Classe Block", () => {
+  let data, lastBlock, block;
+
+  beforeEach(() => {
+    data = "Dados do bloco";
+    lastBlock = Block.genesis();
+    block = Block.mine(lastBlock, data);
+  });
+
+  describe("construtor", () => {
+    it("bloco deve ter a estrutura de dados correta", () => {
+      expect(testBlockDataStructure(block)).toBe(true);
+    });
+  });
+
+  describe("método genesis", () => {
+    it("bloco genesis deve ter a estrutura de dados correta", () => {
+      expect(testGenesisBlockStructure(Block.genesis())).toBe(true);
+    });
+  });
+
+  describe("método getBlockHash", () => {
+    it("dado um bloco, deve retornar seu hash corretamente", () => {
+      expect(Block.getBlockHash(block)).toBe(block.hash);
+    });
+  });
+
+  describe("método mine", () => {
+    it("dados do bloco minerado devem ser iguais aos dados passados", () => {
+      expect(block.data).toEqual(data);
+    });
+
+    it("bloco anterior deve estar correto", () => {
+      expect(block.lastHash).toEqual(lastBlock.hash);
+    });
+  });
+});
+
+const testBlockDataStructure = block =>
+  typeof block.timestamp === "number" &&
+  typeof block.hash === "string" &&
+  typeof block.lastHash === "string" &&
+  typeof block.data === "string";
+
+const testGenesisBlockStructure = genesis =>
+  typeof genesis.timestamp === "number" &&
+  typeof genesis.hash === "string" &&
+  genesis.lastHash === null &&
+  typeof genesis.data === "string";
---
+++ src/block/index.js
@@ -0,0 +1,50 @@
+const sha256 = require("crypto-js/sha256");
+
+class Block {
+  constructor(timestamp, lastHash, hash, data) {
+    this.timestamp = timestamp;
+    this.lastHash = lastHash;
+    this.hash = hash;
+    this.data = data;
+  }
+
+  toString() {
+    return `
+    Bloco
+
+      Criado em:      ${this.timestamp}
+      Bloco anterior: ${this.lastHash}
+      Hash:           ${this.hash}
+      Dados:          ${this.data}
+    `;
+  }
+
+  static genesis() {
+    const timestamp = Date.now();
+    const lastHash = null;
+    const data = "Genesis";
+    const hash = Block.hash(timestamp, lastHash, data);
+
+    return new Block(timestamp, lastHash, hash, data);
+  }
+
+  static getBlockHash(block) {
+    const { timestamp, lastHash, data } = block;
+
+    return Block.hash(timestamp, lastHash, data);
+  }
+
+  static hash(timestamp, lastHash, data) {
+    return sha256(`${timestamp}-${lastHash}-${data}`).toString();
+  }
+
+  static mine(lastBlock, data) {
+    const timestamp = Date.now();
+    const lastHash = lastBlock.hash;
+    const hash = Block.hash(timestamp, lastHash, data);
+
+    return new Block(timestamp, lastHash, hash, data);
+  }
+}
+
+module.exports = Block;
