ef9448b7746f7987b9eb78e4a010c5be907b6139
Pedro Reis <pedroreisv@gmail.com>
2018-06-16 11:19:40 -0300

    Add wallet persistence

--- src/constants.js
+++ src/constants.js
@@ -1,12 +1,12 @@
-const isProduction = process.env.NODE_ENV === 'production';
+const { isProduction } = require('./utilities');
 
 const SECOND = 1000;
 const MINUTE = 60 * SECOND;
 
-const BASE_DIFFICULTY = isProduction ? 4 : 2;
-const MINE_RATE = isProduction ? 1 * MINUTE : 15 * SECOND;
+const BASE_DIFFICULTY = isProduction() ? 4 : 2;
+const MINE_RATE = isProduction() ? 1 * MINUTE : 15 * SECOND;
 
-const INITIAL_BALANCE = isProduction ? 100 : 1000;
+const INITIAL_BALANCE = isProduction() ? 100 : 1000;
 const MINING_REWARD = 10;
 
 module.exports = {
--- src/utilities/index.js
+++ src/utilities/index.js
@@ -4,14 +4,18 @@ const SHA256 = require('crypto-js/sha256');
 
 const ec = new EC('secp256k1');
 const genKeyPair = () => ec.genKeyPair();
+const genKeyPairFromPrivate = privateKey => ec.keyFromPrivate(privateKey, 'hex');
 const genUniqueId = () => uuid();
 const hashData = data => SHA256(JSON.stringify(data)).toString();
 const isSignatureValid = (publicKey, signature, dataHash) =>
   ec.keyFromPublic(publicKey, 'hex').verify(dataHash, signature);
+const isProduction = () => process.env.NODE_ENV === 'production';
 
 module.exports = {
   genKeyPair,
+  genKeyPairFromPrivate,
   genUniqueId,
   hashData,
   isSignatureValid,
+  isProduction,
 };
