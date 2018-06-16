26d5f735f2b22d78c96f9f288cd6af83d3e12d52
Pedro Reis <pedroreisv@gmail.com>
2018-06-16 14:53:24 -0300

    Add Header component

---
+++ src/client/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => (
+  <nav className="navbar is-dark">
+    <div className="navbar-brand">
+      <a className="navbar-item is-size-3 has-text-white has-text-weight-bold" href="/">
+        Rio Coin
+      </a>
+    </div>
+  </nav>
+);
+
+export default Header;
