7822760fd56aab1c474afb68470ec7ab9333126c
Pedro Reis <pedroreisv@gmail.com>
2018-06-16 14:53:41 -0300

    Add Wallet and refactor App root

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Header from './components/Header';
+import Wallet from './components/Wallet';
 
-const App = () => <div>Rio Coin</div>;
+const App = () => (
+  <div>
+    <Header />
+    <div className="container">
+      <Wallet />
+    </div>
+  </div>
+);
 
 ReactDOM.render(<App />, document.getElementById('app'));
