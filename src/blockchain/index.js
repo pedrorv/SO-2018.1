6039bc9adf771c0f166310317e48e482ee841f98
Pedro Reis <pedroreisv@gmail.com>
2018-06-18 10:45:54 -0300

    Fix reduces

--- src/blockchain/index.js
+++ src/blockchain/index.js
@@ -21,37 +21,47 @@ class Blockchain {
 
   isChainValid(chain) {
     const listBlocks = chain.reduce((acc, block) => [...acc, ...block.hash], []);
-    var newBlockChain = new Blockchain();
+    const newBlockChain = new Blockchain();
     newBlockChain.chain = chain;
     return chain.every((block, index) => {
       if (index === 0) {
         return JSON.stringify(chain[0]) === JSON.stringify(Block.genesis());
       }
 
-      //validar as transações dentro da blockchain
-      //verificar se bloco está na blockchain atual
-      //se não estiver verificar os saldos dos inputs [utilizar metodo blockchain.getBalance(blockchain, currenttimestamp = new Date())]
-      //e se soma dos inputs bate com a soma dos outputs
-      if (!listBlocks.includes(block.hash)){
+      // validar as transações dentro da blockchain
+      // verificar se bloco está na blockchain atual
+      // se não estiver verificar os saldos dos inputs [utilizar metodo blockchain.getBalance(blockchain, currenttimestamp = new Date())]
+      // e se soma dos inputs bate com a soma dos outputs
+      if (!listBlocks.includes(block.hash)) {
         const transactions = block.data;
-        transactions.forEach(transaction => {
-          if (transaction.input.amount != newBlockChain.getBalance(transaction.input.address), transaction.input.timestamp)
+        transactions.forEach((transaction) => {
+          if (
+            (transaction.input.amount !== newBlockChain.getBalance(transaction.input.address),
+            transaction.input.timestamp)
+          ) {
             return false;
-          if (transaction.input.amount != transaction.outputs.reduce((amt, o) => amt += o.amount, 0 ))
+          }
+          if (
+            transaction.input.amount != transaction.outputs.reduce((amt, o) => amt + o.amount, 0)
+          ) {
             return false;
+          }
         });
       }
-       const lastBlock = chain[index - 1];
-      return (
-        block.lastHash === lastBlock.hash &&
-        block.hash === Block.getBlockHash(block)
-      );
+      const lastBlock = chain[index - 1];
+      return block.lastHash === lastBlock.hash && block.hash === Block.getBlockHash(block);
     });
   }
 
   replaceChain(newChain) {
-    const actualChainDifficulty = this.chain.reduce((acc, block) => acc += Math.pow(2,block.difficulty), 0);
-    const newChainDifficulty = this.chain.reduce((acc, block) => acc += Math.pow(2,block.difficulty), 0);
+    const actualChainDifficulty = this.chain.reduce(
+      (acc, block) => acc + Math.pow(2, block.difficulty),
+      0,
+    );
+    const newChainDifficulty = this.chain.reduce(
+      (acc, block) => acc + Math.pow(2, block.difficulty),
+      0,
+    );
 
     if (newChainDifficulty <= actualChainDifficulty) {
       console.log('A nova blockchain não é maior que a blockchain atual.');
@@ -67,17 +77,21 @@ class Blockchain {
     this.chain = newChain;
   }
 
-  getBalance( address, currenttimestamp = new Date()) {
-
+  getBalance(address, currenttimestamp = new Date()) {
+    let balance;
     const transactions = this.chain.reduce((acc, block) => [...acc, ...block.data], []);
-    const tempestiveTransactions = transactions.filter(t => t.input.timestamp < currenttimestamp );
+    const tempestiveTransactions = transactions.filter(t => t.input.timestamp < currenttimestamp);
     const walletInputs = transactions.filter(t => t.input.address === address);
     let mostRecentTimestamp = 0;
     balance = INITIAL_BALANCE;
+
     if (walletInputs.length) {
       const mostRecentInput = walletInputs.reduce((acc, cur) => (acc.input.timestamp > cur.input.timestamp ? acc : cur));
 
-      balance = mostRecentInput.outputs.reduce((sum, o) => (o.address === address ? sum + o.amount : sum), 0);
+      balance = mostRecentInput.outputs.reduce(
+        (sum, o) => (o.address === address ? sum + o.amount : sum),
+        0,
+      );
 
       mostRecentTimestamp = mostRecentInput.input.timestamp;
     }
@@ -85,8 +99,7 @@ class Blockchain {
     return tempestiveTransactions.reduce((acc, t) => {
       if (t.input.timestamp > mostRecentTimestamp) {
         return (
-          acc +
-          t.outputs.reduce((sum, o) => (o.address === address ? sum + o.amount : sum), 0)
+          acc + t.outputs.reduce((sum, o) => (o.address === address ? sum + o.amount : sum), 0)
         );
       }
 
